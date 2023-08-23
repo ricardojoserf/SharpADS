14da6119a21c4c4459ab98a05da517352cf57ebf
Ricardo Ruiz <ricardojruiz93@gmail.com>
2023-08-23 12:27:16 +0200

    Add files via upload

--- SharpADS/Program.cs
+++ SharpADS/Program.cs
@@ -1,86 +1,118 @@
 ﻿using System;
-using System.IO;
-using System.Net;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using System.Runtime.InteropServices;
+
 
 namespace SharpADS
 {
     class Program
     {
-        // access: https://learn.microsoft.com/en-us/windows/win32/fileio/file-access-rights-constants
-        // access: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-samr/262970b7-cd4a-41f4-8c4d-5a27f0092aaa
-        // share, creationDisposition, flagsAndAttributes: https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
-        static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
-        const int MAX_PATH = 260;
-        const int ADS_MAX_SIZE = 0x40000 - 1;
-
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)] public static extern IntPtr CreateFileW([MarshalAs(UnmanagedType.LPWStr)] string filename, uint access, int share, IntPtr securityAttributes, int creationDisposition, int flagsAndAttributes, IntPtr templateFile);
         [DllImport("kernel32.dll")] static extern bool WriteFile(IntPtr hFile, byte[] lpBuffer, int nNumberOfBytesToWrite, out uint lpNumberOfBytesWritten, [In] IntPtr lpOverlapped); [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)] public struct WIN32_FIND_STREAM_DATA { public long StreamSize; [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MAX_PATH + 36)] public string StreamName; }
         [DllImport("kernel32.dll", SetLastError = true)] static extern bool ReadFile(IntPtr hFile, [Out] byte[] lpBuffer, uint nNumberOfBytesToRead, out uint lpNumberOfBytesRead, IntPtr lpOverlapped);
-        [DllImport("kernel32.dll", SetLastError = true)] [return: MarshalAs(UnmanagedType.Bool)] static extern bool DeleteFile(string lpFileName);
+        [DllImport("kernel32.dll", SetLastError = true)][return: MarshalAs(UnmanagedType.Bool)] static extern bool DeleteFile(string lpFileName);
         [DllImport("kernel32.dll", SetLastError = true)][return: MarshalAs(UnmanagedType.Bool)] static extern bool CloseHandle(IntPtr hObject);
         [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)] public static extern IntPtr FindFirstStreamW([MarshalAs(UnmanagedType.LPTStr)] string filename, IntPtr infoLevel, out WIN32_FIND_STREAM_DATA data, int reserved = 0);
         [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)] public static extern bool FindNextStreamW(IntPtr hFind, out WIN32_FIND_STREAM_DATA data);
-        
-        
-        static void listStreams(String filename) {
-            Console.WriteLine("[+] Listing streams \n");
-            WIN32_FIND_STREAM_DATA data2;
-            IntPtr hFind = FindFirstStreamW(filename, IntPtr.Zero, out data2);
+        [DllImport("kernel32.dll")] static extern uint GetLastError();
+     
+        // Source: https://learn.microsoft.com/en-us/windows/win32/fileio/file-access-rights-constants
+        // Source: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-samr/262970b7-cd4a-41f4-8c4d-5a27f0092aaa
+        // Source: https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-createfilea
+        static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
+        const int MAX_PATH = 260;
+        const uint GENERIC_READ = 0x80000000;
+        const uint GENERIC_WRITE = 0x40000000;
+        const int FILE_WRITE_ATTRIBUTES = 0x100;
+        const int FILE_SHARE_READ = 0x00000001;
+        const int OPEN_ALWAYS = 4;
+        const int FILE_FLAG_SEQUENTIAL_SCAN = 0x80;
+        const int FILE_ATTRIBUTE_NORMAL = 0x08000000;
+        // Maximum size to display for every ADS value
+        const int MAX_ADS_SIZE_TO_DISPLAY = 256;
+
+
+        static void listStreams(String filename)
+        {
+            Console.WriteLine("[+] Listing streams...\n");
+            WIN32_FIND_STREAM_DATA stream_data;
+            IntPtr hFind = FindFirstStreamW(filename, IntPtr.Zero, out stream_data, 0);
 
             if (hFind == INVALID_HANDLE_VALUE)
             {
-                Console.WriteLine("[-] Invalid handle (hFind)");
-                System.Environment.Exit(-1);
+                uint lasterror = GetLastError();
+                if (lasterror == 38)
+                {
+                    Console.WriteLine("[+] No ADS values in this directory");
+                    return;
+                }
+                else if (lasterror == 2) {
+                    Console.WriteLine("[-] File or directory not found: " + filename);
+                    System.Environment.Exit(-1);
+                }
+                else
+                {
+                    Console.WriteLine("[-] Invalid handle. Error code: " + GetLastError());
+                    System.Environment.Exit(-1);
+                }
             }
 
+            int count = 0;
             while (true)
             {
-                if (data2.StreamName != "::$DATA")
+                if (stream_data.StreamName != "::$DATA")
                 {
-                    String stream_name_aux = filename + data2.StreamName;
-                    byte[] data3 = new byte[256];
-                    IntPtr streamHandle_aux = CreateFileW(stream_name_aux, 0x80000000, 0x00000001, IntPtr.Zero, 4, 0x80 | 0x08000000, IntPtr.Zero);
+                    count += 1;
+                    String stream_name_aux = filename + stream_data.StreamName;
+                    byte[] stream_content = new byte[MAX_ADS_SIZE_TO_DISPLAY];
+                    //IntPtr streamHandle_aux = CreateFileW(stream_name_aux, 0x80000000, 0x00000001, IntPtr.Zero, 4, 0x80 | 0x08000000, IntPtr.Zero);
+                    IntPtr streamHandle_aux = CreateFileW(stream_name_aux, GENERIC_READ, FILE_SHARE_READ, IntPtr.Zero, OPEN_ALWAYS, FILE_FLAG_SEQUENTIAL_SCAN | FILE_ATTRIBUTE_NORMAL, IntPtr.Zero);
 
                     if (streamHandle_aux == INVALID_HANDLE_VALUE)
                     {
-                        Console.WriteLine("[-] Invalid handle (streamHandle_aux)");
+                        Console.WriteLine("[-] Invalid handle. Error code: " + GetLastError());
                         System.Environment.Exit(-1);
                     }
 
-                    ReadFile(streamHandle_aux, data3, (uint)data3.Length, out uint data4, IntPtr.Zero);
+                    ReadFile(streamHandle_aux, stream_content, (uint)stream_content.Length, out uint _, IntPtr.Zero);
                     Console.WriteLine("[+] Stream name: " + stream_name_aux);
-                    Console.WriteLine("[+] ADS content: " + Encoding.UTF8.GetString(data3, 0, data3.Length));
+                    Console.WriteLine("[+] ADS content: " + Encoding.UTF8.GetString(stream_content, 0, stream_content.Length));
                 }
-                if (!FindNextStreamW(hFind, out data2))
+                if (!FindNextStreamW(hFind, out stream_data))
                 {
                     break;
                 }
+            }
+
+            if (count != 0)
+            {
+                Console.WriteLine("[+] Number of ADS values: " + count);
+            }
+            else {
+                Console.WriteLine("[+] No ADS values in this file");
             }
         }
 
 
         static void clearStreams(String filename)
         {
-            Console.WriteLine("[+] Deleting all streams");
-            WIN32_FIND_STREAM_DATA data2;
-            IntPtr hFind = FindFirstStreamW(filename, IntPtr.Zero, out data2);
+            Console.WriteLine("[+] Deleting all streams...\n");
+            WIN32_FIND_STREAM_DATA stream_data;
+            IntPtr hFind = FindFirstStreamW(filename, IntPtr.Zero, out stream_data);
 
             if (hFind == INVALID_HANDLE_VALUE)
             {
-                Console.WriteLine("[-] Invalid handle (hFind)");
+                Console.WriteLine("[-] Invalid handle. Error code: " + GetLastError());
                 System.Environment.Exit(-1);
             }
 
             while (true)
             {
-                if (data2.StreamName != "::$DATA") {
-                    String stream_name_aux = filename + data2.StreamName;
+                if (stream_data.StreamName != "::$DATA") {
+                    String stream_name_aux = filename + stream_data.StreamName;
                     DeleteFile(stream_name_aux);
                 }
-                if (!FindNextStreamW(hFind, out data2))
+                if (!FindNextStreamW(hFind, out stream_data))
                 {
                     break;
                 }
@@ -88,26 +120,27 @@ namespace SharpADS
         }
 
 
-        static void writeStream(String filename, String stream_name, byte[] data) {
-            Console.WriteLine("[+] Creating or updating stream "+ stream_name + " \n");
+        static void writeStream(String filename, String stream_name, byte[] payload_bytes)
+        {
+            Console.WriteLine("[+] Creating or updating stream "+ stream_name + " ...");
             String target_stream = $"{filename}:{stream_name}";
             DeleteFile(target_stream);
-            IntPtr streamHandle = CreateFileW(target_stream, 0x80000000 | 0x40000000 | 0x100, 0x00000001, IntPtr.Zero, 4, 0x80 | 0x08000000, IntPtr.Zero);
+            IntPtr streamHandle = CreateFileW(target_stream, GENERIC_READ | GENERIC_WRITE | FILE_WRITE_ATTRIBUTES, FILE_SHARE_READ, IntPtr.Zero, OPEN_ALWAYS, FILE_FLAG_SEQUENTIAL_SCAN | FILE_ATTRIBUTE_NORMAL, IntPtr.Zero);
 
             if (streamHandle == INVALID_HANDLE_VALUE)
             {
-                Console.WriteLine("[-] Invalid handle (streamHandle)");
+                Console.WriteLine("[-] Invalid handle. Error code: " + GetLastError());
                 System.Environment.Exit(-1);
             };
 
-            WriteFile(streamHandle, data, data.Length, out uint aux, IntPtr.Zero);
+            WriteFile(streamHandle, payload_bytes, payload_bytes.Length, out uint aux, IntPtr.Zero);
             CloseHandle(streamHandle);
         }
 
 
         static void deleteStream(String filename, String stream_name)
         {
-            Console.WriteLine("[+] Deleting stream "+stream_name+" \n");
+            Console.WriteLine("[+] Deleting stream "+stream_name+" ...");
             String target_stream = $"{filename}:{stream_name}";
             DeleteFile(target_stream);
         }
@@ -115,35 +148,36 @@ namespace SharpADS
 
         static void readStream(String filename, String stream_name)
         {
-            Console.WriteLine("[+] Reading stream "+ stream_name + " \n");
-            WIN32_FIND_STREAM_DATA data2;
-            IntPtr hFind = FindFirstStreamW(filename, IntPtr.Zero, out data2);
+            Console.WriteLine("[+] Reading stream "+ stream_name + " ...");
+            WIN32_FIND_STREAM_DATA stream_data;
+            IntPtr hFind = FindFirstStreamW(filename, IntPtr.Zero, out stream_data);
 
             if (hFind == INVALID_HANDLE_VALUE)
             {
-                Console.WriteLine("[-] Invalid handle (hFind)");
+                Console.WriteLine("[-] Invalid handle. Error code: " + GetLastError());
                 System.Environment.Exit(-1);
             }
 
             while (true)
             {
-                if (data2.StreamName == (":"+stream_name+":$DATA"))
+                if (stream_data.StreamName == (":"+stream_name+":$DATA"))
                 {
-                    String stream_name_aux = filename + data2.StreamName;
-                    byte[] data3 = new byte[256];
-                    IntPtr streamHandle_aux = CreateFileW(stream_name_aux, 0x80000000, 0x00000001, IntPtr.Zero, 4, 0x80 | 0x08000000, IntPtr.Zero);
+                    String stream_name_aux = filename + stream_data.StreamName;
+                    byte[] stream_content = new byte[MAX_ADS_SIZE_TO_DISPLAY];
+                    // IntPtr streamHandle_aux = CreateFileW(stream_name_aux, 0x80000000, 0x00000001, IntPtr.Zero, 4, 0x80 | 0x08000000, IntPtr.Zero);
+                    IntPtr streamHandle_aux = CreateFileW(stream_name_aux, GENERIC_READ, FILE_SHARE_READ, IntPtr.Zero, OPEN_ALWAYS, FILE_FLAG_SEQUENTIAL_SCAN | FILE_ATTRIBUTE_NORMAL, IntPtr.Zero);
 
                     if (streamHandle_aux == INVALID_HANDLE_VALUE)
                     {
-                        Console.WriteLine("[-] Invalid handle (streamHandle_aux)");
+                        Console.WriteLine("[-] Invalid handle. Error code: " + GetLastError());
                         System.Environment.Exit(-1);
                     }
 
-                    ReadFile(streamHandle_aux, data3, (uint)data3.Length, out uint data4, IntPtr.Zero);
+                    ReadFile(streamHandle_aux, stream_content, (uint)stream_content.Length, out uint _, IntPtr.Zero);
                     Console.WriteLine("[+] Stream name: " + stream_name_aux);
-                    Console.WriteLine("[+] ADS content: " + Encoding.UTF8.GetString(data3, 0, data3.Length));
+                    Console.WriteLine("[+] ADS content: " + Encoding.UTF8.GetString(stream_content, 0, stream_content.Length));
                 }
-                if (!FindNextStreamW(hFind, out data2))
+                if (!FindNextStreamW(hFind, out stream_data))
                 {
                     break;
                 }
@@ -164,17 +198,19 @@ namespace SharpADS
         {
             byte[] buf = { };
 
-            // Payload from url, http or https
+            // Hexadecimal payload
             if (payload_str.Length >= 2)
             {
                 if (payload_str.Substring(0, 2) == "0x")
                 {
-                    try {
+                    try
+                    {
                         payload_str = payload_str.Replace("0x", "");
                         buf = ToByteArray(payload_str);
                         return buf;
                     }
-                    catch (Exception ex){
+                    catch (Exception ex)
+                    {
                         Console.WriteLine(ex.ToString());
                         System.Environment.Exit(-1);
                     }
@@ -182,7 +218,8 @@ namespace SharpADS
             }
 
             // Payload from url, http or https
-            if (payload_str.Length >= 4) {
+            if (payload_str.Length >= 4)
+            {
                 if (payload_str.Substring(0, 4) == "http")
                 {
                     Console.WriteLine("[+] Getting payload from url: " + payload_str);
@@ -202,10 +239,10 @@ namespace SharpADS
                             System.Environment.Exit(-1);
                         }
                     }
-
                 }    
             }
 
+            // Regular payload
             buf = Encoding.ASCII.GetBytes(payload_str);
             return buf;
         }
@@ -217,17 +254,17 @@ namespace SharpADS
             Console.WriteLine("");
 
             Console.WriteLine("[+] SharpADS.exe write FILE_PATH STREAM_NAME PAYLOAD");
-            Console.WriteLine("[+] Example: SharpADS.exe write c:\\Temp\\test.txt ads_name 'This is a text'");
-            Console.WriteLine("[+] Example: SharpADS.exe write c:\\Temp\\test.txt ads_name http:///127.0.0.1/payload.bin");
-            Console.WriteLine("[+] Example: SharpADS.exe write c:\\Temp\\test.txt ads_name 4142434445");
+            Console.WriteLine("[+] Example (string):      SharpADS.exe write c:\\Temp\\test.txt ADS_name1 RandomString");
+            Console.WriteLine("[+] Example (hexadecimal): SharpADS.exe write c:\\Temp\\test.txt ADS_name2 0x4142434445");
+            Console.WriteLine("[+] Example (from url):    SharpADS.exe write c:\\Temp\\test.txt ADS_name3 http:///127.0.0.1/payload.bin");
             Console.WriteLine("");
 
             Console.WriteLine("[+] SharpADS.exe read FILE_PATH STREAM_NAME");
-            Console.WriteLine("[+] Example: SharpADS.exe read c:\\Temp\\test.txt ads_name");
+            Console.WriteLine("[+] Example: SharpADS.exe read c:\\Temp\\test.txt ADS_name1");
             Console.WriteLine("");
 
             Console.WriteLine("[+] SharpADS.exe delete FILE_PATH STREAM_NAME");
-            Console.WriteLine("[+] Example: SharpADS.exe delete c:\\Temp\\test.txt ads_name");
+            Console.WriteLine("[+] Example: SharpADS.exe delete c:\\Temp\\test.txt ADS_name1");
             Console.WriteLine("");
 
             Console.WriteLine("[+] SharpADS.exe list FILE_PATH");
@@ -243,22 +280,31 @@ namespace SharpADS
 
         static void Main(string[] args)
         {
-            if (args.Length < 2) {
+            if (args.Length < 2)
+            {
                 getHelp();
             }
-            String option = args[0];
 
+            String option = args[0];
+            String filename = args[1];
+            filename = filename.Replace("'", "");
+            filename = filename.Replace("\"", "");
+            if (filename.EndsWith("\\"))
+            {
+            
+                filename = filename.Remove(filename.Length - 1, 1);
+            }
+            
             if (option == "write")
             {
                 if (args.Length < 4)
                 {
                     getHelp();
                 }
-                String filename = args[1];
                 String stream_name = args[2];
                 String payload = args[3];
-                byte[] data = getPayload(payload);
-                writeStream(filename, stream_name, data);
+                byte[] payload_content = getPayload(payload);
+                writeStream(filename, stream_name, payload_content);
                 listStreams(filename);
             }
 
@@ -268,7 +314,6 @@ namespace SharpADS
                 {
                     getHelp();
                 }
-                String filename = args[1];
                 String stream_name = args[2];
                 deleteStream(filename, stream_name);
                 listStreams(filename);
@@ -280,20 +325,19 @@ namespace SharpADS
                 {
                     getHelp();
                 }
-                String filename = args[1];
                 String stream_name = args[2];
                 readStream(filename, stream_name);
             }
 
-            else if (option == "list") {
-                String filename = args[1];
+            else if (option == "list")
+            {
                 listStreams(filename);
             }
             
             else if (option == "clear")
             {
-                String filename = args[1];
                 clearStreams(filename);
+                listStreams(filename);
             }
 
         }
